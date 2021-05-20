09ad287fd88c8317f92baed926bdcd638d075107
ludo loops <ludoloops@protonmail.com>
2021-05-20 17:42:26 +0200

    Feat: style component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,31 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styled from "styled-components"
 const about = () => {
-  return <Layout>hello from about page</Layout>
-}
+  return (
+    <Layout>
+      <Wrapper>
+        <h1>hello from about page</h1>
 
+        <p className="text">
+          {/*  spell-checker: disable   */}
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
+          repellendus. Reprehenderit maxime quae similique, tempora quasi, porro
+          in recusandae eos voluptatibus dolorum qui eligendi laborum ea
+          blanditiis earum, praesentium consequatur?
+          {/* spell-checker: enable  */}
+        </p>
+      </Wrapper>
+    </Layout>
+  )
+}
+const Wrapper = styled.section`
+  color: blueviolet;
+  h1 {
+    color: yellowgreen;
+  }
+  .text {
+    text-transform: uppercase;
+  }
+`
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
-
+import { ExampleButton } from "../examples/button"
 export default function Home() {
   return (
     <Layout>
-      <h1>Hello page</h1>
+      <div className="home-heading">
+        <ExampleButton>click me</ExampleButton>
+        <h1>Hello page</h1>
+      </div>
     </Layout>
   )
 }
