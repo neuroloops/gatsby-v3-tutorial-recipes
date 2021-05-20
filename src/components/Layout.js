8f2929fef276fdbe2053e06d983ec4c7c06afa75
ludo loops <ludoloops@protonmail.com>
2021-05-20 17:58:06 +0200

    Feat: css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,10 @@ import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 
+import "normalize.css"
+
+import "../assets/css/main.css"
+
 const Layout = ({ children }) => {
   return (
     <>
