3659d36a04039e2ef360fbcb1f9d6b146a007f1e
ludo loops <ludoloops@protonmail.com>
2021-05-21 17:38:42 +0200

    Feat: about page with image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,38 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
-const about = () => {
+const About = () => {
   return (
     <Layout>
-      <h1>hello from about page</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm a baby coloring book poke taxidermy</h2>
+            <p>
+              Copper mug vaporware marfa kickstarter iceland. Listicle tacos
+              humblebrag polaroid typewriter kale chips viral activated charcoal
+              air plant forage 8-bit pabst.
+            </p>
+            <p>
+              Cloud bread blue bottle shabby chic 90's deep v jean shorts edison
+              bulb tumeric vaporware pour-over retro PBR&B quinoa.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default about
+export default About
