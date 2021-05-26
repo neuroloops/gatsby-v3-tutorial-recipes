f322634c061d7fe120dc8fdb522ca50a36c8fad5
ludo loops <ludoloops@protonmail.com>
2021-05-27 00:23:15 +0200

    Feat: all recipes compoenent

---
+++ src/components/AllRecipes.js
@@ -0,0 +1,43 @@
+import React from "react"
+import RecipesList from "./RecipesList"
+import TagsList from "./TagsList"
+import { graphql, useStaticQuery } from "gatsby"
+
+const query = graphql`
+  {
+    allContentfulRecipe(sort: { fields: title, order: ASC }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
+
+const AllRecipes = () => {
+  const data = useStaticQuery(query)
+  const recipes = data.allContentfulRecipe.nodes
+
+  // shortcut :
+  // const {
+  //   allContentfulRecipe: { node: recipes },
+  // } = useStaticQuery(query)
+
+  return (
+    <div>
+      <h4>All recipes</h4>
+      <TagsList recipes={recipes} />
+      <RecipesList recipes={recipes} />
+    </div>
+  )
+}
+
+export default AllRecipes
